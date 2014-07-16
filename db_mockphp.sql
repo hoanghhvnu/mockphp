d2c7696e26cfe5c63324c3680c5690f5cff1f329
hoanghh <hoanghh.vnu@gmail.com>
2014-07-16 23:29:28 +0700

    init repo, sql source file

---
+++ db_mockphp.sql
@@ -0,0 +1,316 @@
+-- MySQL dump 10.13  Distrib 5.5.8, for Win32 (x86)
+--
+-- Host: localhost    Database: mockproject
+-- ------------------------------------------------------
+-- Server version	5.5.8
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tbl_bran`
+--
+
+DROP TABLE IF EXISTS `tbl_bran`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_bran` (
+  `bran_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `bran_name` varchar(200) NOT NULL,
+  PRIMARY KEY (`bran_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_bran`
+--
+
+LOCK TABLES `tbl_bran` WRITE;
+/*!40000 ALTER TABLE `tbl_bran` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_bran` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_category`
+--
+
+DROP TABLE IF EXISTS `tbl_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_category` (
+  `cate_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `cate_name` varchar(50) NOT NULL,
+  `cate_parent` int(10) NOT NULL DEFAULT '0',
+  `cate_order` int(5) NOT NULL,
+  PRIMARY KEY (`cate_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_category`
+--
+
+LOCK TABLES `tbl_category` WRITE;
+/*!40000 ALTER TABLE `tbl_category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_cateproduct`
+--
+
+DROP TABLE IF EXISTS `tbl_cateproduct`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_cateproduct` (
+  `cate_id` int(10) NOT NULL,
+  `pro_id` int(10) NOT NULL,
+  `catepro_order` int(3) NOT NULL,
+  PRIMARY KEY (`cate_id`,`pro_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_cateproduct`
+--
+
+LOCK TABLES `tbl_cateproduct` WRITE;
+/*!40000 ALTER TABLE `tbl_cateproduct` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_cateproduct` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_comment`
+--
+
+DROP TABLE IF EXISTS `tbl_comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_comment` (
+  `com_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `com_author` varchar(50) NOT NULL,
+  `com_email` varchar(50) NOT NULL,
+  `com_content` text NOT NULL,
+  `com_score` float NOT NULL,
+  `com_status` tinyint(1) NOT NULL,
+  `pro_id` int(10) NOT NULL,
+  PRIMARY KEY (`com_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_comment`
+--
+
+LOCK TABLES `tbl_comment` WRITE;
+/*!40000 ALTER TABLE `tbl_comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_country`
+--
+
+DROP TABLE IF EXISTS `tbl_country`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_country` (
+  `coun_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `coun_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`coun_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_country`
+--
+
+LOCK TABLES `tbl_country` WRITE;
+/*!40000 ALTER TABLE `tbl_country` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_country` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_images`
+--
+
+DROP TABLE IF EXISTS `tbl_images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_images` (
+  `img_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `img_name` varchar(50) NOT NULL,
+  `img_title` varchar(200) NOT NULL,
+  `img_status` tinyint(1) NOT NULL DEFAULT '1',
+  `pro_id` int(10) NOT NULL,
+  PRIMARY KEY (`img_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_images`
+--
+
+LOCK TABLES `tbl_images` WRITE;
+/*!40000 ALTER TABLE `tbl_images` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_images` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_order`
+--
+
+DROP TABLE IF EXISTS `tbl_order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_order` (
+  `order_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `cus_name` varchar(50) NOT NULL,
+  `cus_email` varchar(50) NOT NULL,
+  `cus_phone` int(15) NOT NULL,
+  `cus_address` varchar(200) NOT NULL,
+  `create_date` datetime NOT NULL,
+  `order_status` tinyint(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_order`
+--
+
+LOCK TABLES `tbl_order` WRITE;
+/*!40000 ALTER TABLE `tbl_order` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_orderdetail`
+--
+
+DROP TABLE IF EXISTS `tbl_orderdetail`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_orderdetail` (
+  `orderdetail_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `pro_name` varchar(200) NOT NULL,
+  `pro_price` float NOT NULL,
+  `pro_number` int(10) NOT NULL,
+  `order_id` int(10) NOT NULL,
+  PRIMARY KEY (`orderdetail_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_orderdetail`
+--
+
+LOCK TABLES `tbl_orderdetail` WRITE;
+/*!40000 ALTER TABLE `tbl_orderdetail` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_orderdetail` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_product`
+--
+
+DROP TABLE IF EXISTS `tbl_product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_product` (
+  `pro_id` int(10) NOT NULL AUTO_INCREMENT,
+  `pro_name` varchar(200) NOT NULL,
+  `pro_price` float NOT NULL,
+  `pro_price_sale` float NOT NULL,
+  `pro_images` varchar(255) NOT NULL,
+  `pro_desc` text NOT NULL,
+  `pro_info` text NOT NULL,
+  `pro_status` tinyint(1) NOT NULL DEFAULT '0',
+  `pro_bran` int(10) NOT NULL,
+  `pro_country` int(10) NOT NULL,
+  PRIMARY KEY (`pro_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_product`
+--
+
+LOCK TABLES `tbl_product` WRITE;
+/*!40000 ALTER TABLE `tbl_product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_slide`
+--
+
+DROP TABLE IF EXISTS `tbl_slide`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_slide` (
+  `slide_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `slide_title` varchar(200) NOT NULL,
+  `slide_desc` text NOT NULL,
+  `slide_name` varchar(255) NOT NULL,
+  `slide_order` int(3) NOT NULL,
+  `slide_status` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`slide_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_slide`
+--
+
+LOCK TABLES `tbl_slide` WRITE;
+/*!40000 ALTER TABLE `tbl_slide` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_slide` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_user`
+--
+
+DROP TABLE IF EXISTS `tbl_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_user` (
+  `usr_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `usr_name` varchar(50) NOT NULL,
+  `usr_password` char(50) NOT NULL,
+  `usr_email` varchar(100) NOT NULL,
+  `usr_address` text NOT NULL,
+  `usr_phone` varchar(15) NOT NULL,
+  `usr_level` char(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`usr_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+LOCK TABLES `tbl_user` WRITE;
+/*!40000 ALTER TABLE `tbl_user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-07-16 23:20:23
